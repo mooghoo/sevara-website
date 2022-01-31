b78591aeb2e4ee9dbcb74e3002738bbc23053c36
mooghoo <albertopablito8@gmail.com>
2022-01-30 19:32:33 -0700

    Final website

--- js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $(document).on('mousemove', function(e) {
-      $('#circularcursor').css({
-        left: e.pageX,
-        top: e.pageY
-      });
-    })
-  });
